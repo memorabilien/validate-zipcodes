fc1fe0275c32777245a0a8cbba3cf502138aed40
memorabilien <management@memorabilien.org>
2023-01-31 10:40:17 +0100

    bugfixes

--- src/postpublish.js
+++ src/postpublish.js
@@ -6,6 +6,13 @@ try{ packageFile = fs.readFileSync("../package.json", "utf-8");}catch(error){con
 
 packageFile = JSON.parse(packageFile);
 version = packageFile.version;
+console.log(version);
 version = version.split(".");
 version[version.length -1] = (parseInt(version[version.length -1]) + 1).toString();
 packageFile.version = version.join(".");
+
+try{
+    fs.writeFileSync("../package.json", JSON.stringify(packageFile), "utf8");
+}catch(error){
+    console.log(error);
+}
